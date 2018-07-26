7215c94dadb74cd53f555c09d208091a455918cb
w3cssthemes <w3cssthemes@gmail.com>
2018-07-26 12:58:39 +0545

    Initial commit

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body>
 
 <div class="w3-display-container" style="margin-bottom:50px">
-  <img src="images/beach3.jpg" style="width:100%">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/beach3.jpg" style="width:100%">
   <div class="w3-display-bottomleft w3-container w3-amber w3-hover-orange w3-hide-small"
    style="bottom:10%;opacity:0.7;width:70%">
   <h2><b>4 Good Reasons<br>For travelling with us</b></h2>
@@ -17,7 +17,7 @@
 <div class="w3-row w3-container" style="margin:50px 0">
 <div class="w3-half w3-container">
   <div class="w3-topbar w3-border-amber">
-    <img src="images/cellphone.jpg" style="width:100%">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/cellphone.jpg" style="width:100%">
     <h2>Smart Vacation</h2>
     <p>Full vacation control from your cell phone.</p>
   </div>
@@ -25,7 +25,7 @@
 
 <div class="w3-half w3-container">
   <div class="w3-topbar w3-border-amber">
-    <img src="images/sealions.jpg" style="width:100%">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/sealions.jpg" style="width:100%">
     <h2>Super Offers</h2>
     <p>Up to 50% offers. Always 25% student offers.</p>
   </div>
@@ -35,7 +35,7 @@
 <div class="w3-row w3-container" style="margin:50px 0">
 <div class="w3-half w3-container">
   <div class="w3-topbar w3-border-orange">
-    <img src="images/truck.jpg" style="width:100%">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/truck.jpg" style="width:100%">
     <h2>Car Rental Included</h2>
     <p>Wherever you travel our car rental is included.</p>
   </div>
@@ -43,7 +43,7 @@
 
 <div class="w3-half w3-container">
   <div class="w3-topbar w3-border-orange">
-    <img src="images/nowornever.jpg" style="width:100%">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/nowornever.jpg" style="width:100%">
     <h2>Realize Your Dreams</h2>
     <p>Don't wait until it is to late.</p>
     </div>
